896b44e9fd97560117c2422c3718eed79e522489
marcello505 <marcellohaddeman@gmail.com>
2019-12-13 10:12:22 +0100

    Part 1 Complete

--- Day2_1202_Program_Alarm/src/Main.java
+++ Day2_1202_Program_Alarm/src/Main.java
@@ -8,13 +8,33 @@ public class Main {
         try{
             File file = new File("./input.txt");
             Scanner reader = new Scanner(file);
-            ArrayList input = new ArrayList<Integer>();
+            ArrayList<Integer> input = new ArrayList<Integer>();
             String[] numbers = reader.next().split(",");
             for(String item : numbers){
                 input.add(Integer.parseInt(item));
             }
             System.out.println("lol");
-
+            int x;
+            int y;
+            int z;
+            for(int i = 0;i < input.size();i+=4){
+                if(input.get(i) == 99 ){
+                    break;
+                }
+                if(input.get(i) == 1){
+                    x = input.get(input.get(i+1));
+                    y = input.get(input.get(i+2));
+                    z = input.get(i+3);
+                    input.set(z,x+y);
+                }
+                else if(input.get(i) == 2){
+                    x = input.get(input.get(i+1));
+                    y = input.get(input.get(i+2));
+                    z = input.get(i+3);
+                    input.set(z,x*y);
+                }
+            }
+            System.out.println(input.get(0));
         }
         catch (Exception e){
             e.printStackTrace();
