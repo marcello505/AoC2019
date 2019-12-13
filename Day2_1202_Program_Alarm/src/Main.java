cef1390ce155a6918b44c3f4334ce0fc10f31abb
marcello505 <marcellohaddeman@gmail.com>
2019-12-13 09:47:16 +0100

    Import input file to ArrayList

--- Day2_1202_Program_Alarm/src/Main.java
+++ Day2_1202_Program_Alarm/src/Main.java
@@ -1,6 +1,23 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try{
+            File file = new File("./input.txt");
+            Scanner reader = new Scanner(file);
+            ArrayList input = new ArrayList<Integer>();
+            String[] numbers = reader.next().split(",");
+            for(String item : numbers){
+                input.add(Integer.parseInt(item));
+            }
+            System.out.println("lol");
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
